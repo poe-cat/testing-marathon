8dcb2fb5026c35c41ad21e62c6dbdf740cb8528c
poe-cat <katarzyna.tramea@gmail.com>
2022-07-24 17:34:05 +0200

    Add mocks for AccountService

--- src/test/java/com/poecat/account/AccountServiceTest.java
+++ src/test/java/com/poecat/account/AccountServiceTest.java
@@ -2,10 +2,15 @@ package com.poecat.account;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AccountServiceTest {
 
@@ -13,13 +18,43 @@ public class AccountServiceTest {
     void getAllActiveAccounts() {
 
         //given
-        AccountRepository accountRepositoryStub = new AccountRepositoryStub();
-        AccountService accountService = new AccountService(accountRepositoryStub);
+        List<Account> accounts = prepareAccountData();
+        AccountRepository accountRepository = mock(AccountRepository.class);
+        AccountService accountService = new AccountService(accountRepository);
+        given(accountRepository.getAllAccounts()).willReturn(accounts);
 
         //when
         List<Account> accountList = accountService.getAllActiveAccounts();
 
         //then
         assertThat(accountList, hasSize(2));
+    }
+
+    @Test
+    void getNoActiveAccounts() {
+
+        //given
+        List<Account> accounts = prepareAccountData();
+        AccountRepository accountRepository = mock(AccountRepository.class);
+        AccountService accountService = new AccountService(accountRepository);
+        given(accountRepository.getAllAccounts()).willReturn(Collections.emptyList());
+
+        //when
+        List<Account> accountList = accountService.getAllActiveAccounts();
+
+        //then
+        assertThat(accountList, hasSize(0));
+    }
+
+    private List<Account> prepareAccountData() {
+        Address address1 = new Address("Kwiatowa", "33/5");
+        Account account = new Account(address1);
+
+        Account account2 = new Account();
+
+        Address address2 = new Address("Różana", "12b");
+        Account account3 = new Account(address2);
+
+        return Arrays.asList(account, account2, account3);
     }
 }
